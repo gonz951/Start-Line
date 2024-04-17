6546197a2a08ec42c9a0387c9f0b84431f8bfae7
jlcastro1877 <jlcastro1877@gmail.com>
2024-04-16 21:11:50 -0600

    Modal contact us done jquery

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -6,36 +6,3 @@ $(document).ready(function () {
     $(".wrapper").css("display", "none");
   });
 });
-
-function SendMail() {
-  event.preventDefault();
-
-  console.log(document.getElementById("name").value);
-  console.log(document.getElementById("email").value);
-  console.log(document.getElementById("phone").value);
-  console.log(document.getElementById("message").value);
-
-  var params = {
-    from_name: document.getElementById("name").value,
-    email_id: document.getElementById("email").value,
-    phone_id: document.getElementById("phone").value,
-    message_id: document.getElementById("message").value,
-  };
-
-  // emailjs
-  //   .Send("service_21dpc0d", "template_rtcsv3k", params)
-  //   .then(function (res) {
-  //     alert("Success!" + res.status);
-  //   });
-
-  emailjs.sendForm("service_21dpc0d", "template_rtcsv3k", this).then(
-    () => {
-      console.log("try to send email");
-      alert("Sent!");
-    },
-    (err) => {
-      btn.value = "Send Email";
-      alert(JSON.stringify(err));
-    }
-  );
-}
